ccc450ef3ab6d84355afda590585327c90da0d41
s222309556 <s222309556@deakin.edu.au>
2024-03-21 16:01:20 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Achini Athukorala" });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
