6d6fcb65252b42116e745125cfacfcebc8556d85
s222309556 <s222309556@deakin.edu.au>
2024-03-21 16:28:56 +1100

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express"); // Import express
 const app = express(); // Create an express app
-const port = 3000; // Set the port
+const port = 3000; // Set the port number for the server
 
-app.set("view engine", "ejs"); // Set the view engine to ejs
+// Set the view engine to ejs
+app.set("view engine", "ejs");
 
+// Define a route for the root URL "/"
 app.get("/", (req, res) => {
-  // Set the route
+  // When a request is made to the root URL, render the index.ejs file
+  // Pass a variable "title" with the value "Achini Athukorala" to the template
   res.render("index", { title: "Achini Athukorala" }); // Render the index.ejs file
 });
 
